a01d0981750867f029ffa548d0599c508ada7b13
golummitesoro <135157849+golummitesoro@users.noreply.github.com>
2023-05-31 22:44:50 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -29,7 +29,10 @@ addLayer("p", {
 
     ,upgrades: {
         11: {
-
+            title: "Make this whatever you want!",
+            description: "Double your point gain.",
+            cost: new Decimal(2),
+        
         },
     },
 })
