f955fbf962b7fdd82828fee7324ccdf9ec2183aa
golummitesoro <135157849+golummitesoro@users.noreply.github.com>
2024-05-04 14:57:06 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -13,32 +13,51 @@ addLayer("p", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
+    gainMult() // Calculate the multiplier for main currency from bonuses
+    { 
         mult = new Decimal(1)
+        if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
         return mult
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
+    gainExp() 
+    { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
+    hotkeys: 
+    [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 
 
-    ,upgrades: {
-        11: {
+    ,upgrades: 
+    {
+        11: 
+        {
             title: "Make this whatever you want!",
             description: "Double your point gain.",
             cost: new Decimal(2),
         },
-        12: {
+        12: 
+        {
             title: "Make this whatever you want!",
             description: "Double your point gain.",
             cost: new Decimal(2),
-        effect() {
+            effect() 
+            {
                 return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
+        13: 
+        {
+            title: "Make this whatever you want!",
+            description: "Double your point gain.",
+            cost: new Decimal(2),
+            effect() 
+            {
+                return player.points.add(1).pow(0.15)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
