c66c001c10f2869b95afb902d5aa1c147d79065b
golummitesoro <135157849+golummitesoro@users.noreply.github.com>
2024-05-04 16:30:48 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -41,7 +41,8 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-	let gain = new Decimal(10)
+	let gain = new Decimal(1)
+	gain = gain.times(10)
 	if (hasUpgrade('t', 11)) gain = gain.times(2)
 	if (hasUpgrade('t', 12)) gain = gain.times(upgradeEffect('t', 12))
 	return gain
