8e16ea1002a0588ce41ad19b3ec1b3cafb406a13
golummitesoro <135157849+golummitesoro@users.noreply.github.com>
2024-05-04 14:49:54 +0200

    upgrade 2

--- js/layers.js
+++ js/layers.js
@@ -32,6 +32,15 @@ addLayer("p", {
             title: "Make this whatever you want!",
             description: "Double your point gain.",
             cost: new Decimal(2),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
+        12: {
+            title: "Make this whatever you want!",
+            description: "Double your point gain.",
+            cost: new Decimal(2),
         
         },
     },
