15f308358c4357b6301f027d17eb32d74f8bfc0f
golummitesoro <135157849+golummitesoro@users.noreply.github.com>
2024-05-23 19:05:01 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("f", {
     name: "focus", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "f", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,7 +8,7 @@ addLayer("f", {
     }},
     color: "#6CEB28",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "magical energy", // Name of prestige currency
+    resource: "focus", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
